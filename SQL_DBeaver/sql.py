16a3576c8870d61ab927fefcb5175a14bd332140
Erika De Giorgi <erikadegiorgi18@gmail.com>
2026-07-05 23:23:06 +0200

    modifica f

--- SQL_DBeaver/sql.py
+++ SQL_DBeaver/sql.py
@@ -52,11 +52,13 @@ cursor.execute("""
     SELECT S.n_sala, SA.capienza, COUNT(B.id_biglietto) AS Biglietti_Venduti,
            (COUNT(B.id_biglietto) * 100.0 / SA.capienza) AS Tasso_Occupazione_Percentuale
     FROM SPETTACOLO AS S
-    LEFT JOIN SALA AS SA ON S.n_sala = SA.id_sala
+    JOIN SALA AS SA ON S.n_sala = SA.id_sala
     LEFT JOIN BIGLIETTO AS B ON S.id_spettacolo = B.id_biglietto
     GROUP BY S.n_sala, SA.capienza
     ORDER BY Tasso_Occupazione_Percentuale DESC
 """)
+
+
 rows3 = cursor.fetchall()
 
 print("\nTasso di occupazione per sala:")
